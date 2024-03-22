5f3ff42b68e00ed049a9a20f6329b73a4702e9b7
EduardoDMarotta <eduardodouradomarotta@gmail.com>
2024-03-22 17:05:24 -0300

    Primeiro Desafio com Ruby

---
+++ teste.rb
@@ -0,0 +1,7 @@
+puts "Bom dia!! Qual o seu nome?"
+name = gets.chomp
+puts "E o seu sobrenome?"
+surname = gets.chomp
+puts "Quantos anos tem?"
+age = gets.chomp
+puts "Olá #{name} #{surname} de #{age} anos! Bem vindo a biblioteca da Dio.me!"
